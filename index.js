596ea8097b54f8cafecc5e55a8acbc004443a174
rokon <rokonrabbi21@gmail.com>
2023-06-10 07:09:39 +0600

    access token generated

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ async function run() {
     const result = await usersClasses.find().toArray();
     res.send(result);
   });
+
+  app.post('/jwt', (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+    res.send({ token })
+  })
+
 //  store users 
 app.post('/users', async (req, res) => {
   const user = req.body;
