5a301deae21642bbd2f8a13c8267be460f57f03c
rokon <rokonrabbi21@gmail.com>
2023-06-10 07:10:57 +0600

    verifyjwt middleware added

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.sgbuojp.mongodb.net/?retryWrites=true&w=majority`;
