b86548312b781b6d70248679360ec71281cdda45
rokon <rokonrabbi21@gmail.com>
2023-06-11 23:29:22 +0600

    findmany data get api done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
-const app = express()
-const cors = require('cors');
-require('dotenv').config()
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -11,21 +11,25 @@ app.use(express.json());
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
-    return res.status(401).send({ error: true, message: 'unauthorized access' });
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
   }
   // bearer token
-  const token = authorization.split(' ')[1];
+  const token = authorization.split(" ")[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ error: true, message: 'unauthorized access' })
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
     }
     req.decoded = decoded;
     next();
-  })
-}
+  });
+};
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.sgbuojp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,112 +38,134 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
-
   const usersClasses = client.db("summerCamp").collection("classes");
   const usersCollection = client.db("summerCamp").collection("users");
   const cartCollection = client.db("summerCamp").collection("carts");
-  
-  app.get('/classes', async (req, res) => {
+
+  app.get("/classes", async (req, res) => {
     const result = await usersClasses.find().toArray();
     res.send(result);
   });
+  app.post("/classes", async (req, res) => {
+    const newItem = req.body;
+    const result = await usersClasses.insertOne(newItem);
+    res.send(result);
+  });
+  // get user throught the email
+  app.get("/instructorClasses", verifyJWT, async (req, res) => {
+    const instructorEmail = req.query.instructorEmail;
+    if (req.decoded.email !== instructorEmail) {
+      return res.status(403).send({ error: true, message: "forbidden access" });
+    }
+    let query = {};
+    if (req.query?.instructorEmail) {
+      query = { instructorEmail: req.query.instructorEmail };
+    }
 
-  app.post('/jwt', (req, res) => {
+    const user = await usersClasses.find(query).toArray();
+
+    res.send(user);
+  });
+
+  app.post("/jwt", (req, res) => {
     const user = req.body;
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "1h",
+    });
 
-    res.send({ token })
-  })
+    res.send({ token });
+  });
 
-//  store users 
-app.post('/users', async (req, res) => {
-  const user = req.body;
-  const query = { email: user.email }
-  const existingUser = await usersCollection.findOne(query);
+  //  store users
+  app.post("/users", async (req, res) => {
+    const user = req.body;
+    const query = { email: user.email };
+    const existingUser = await usersCollection.findOne(query);
 
-  if (existingUser) {
-    return res.send({ message: 'user already exists' })
-  }
+    if (existingUser) {
+      return res.send({ message: "user already exists" });
+    }
 
-  const result = await usersCollection.insertOne(user);
-  res.send(result);
-});
+    const result = await usersCollection.insertOne(user);
+    res.send(result);
+  });
 
-// get users 
-  app.get('/users',  async (req, res) => {
+  // get users
+  app.get("/users", async (req, res) => {
     const result = await usersCollection.find().toArray();
     res.send(result);
   });
-  // post card data 
-  app.post('/carts', async (req, res) => {
+  // post card data
+  app.post("/carts", async (req, res) => {
     const item = req.body;
     const result = await cartCollection.insertOne(item);
     res.send(result);
-  })
+  });
   // cart collection apis
-  app.get('/carts', verifyJWT, async (req, res) => {
+  app.get("/carts", verifyJWT, async (req, res) => {
     const email = req.query.email;
-    
+
     if (!email) {
       res.send([]);
     }
 
     const decodedEmail = req.decoded.email;
     if (email !== decodedEmail) {
-      return res.status(403).send({ error: true, message: 'forbidden access' })
+      return res.status(403).send({ error: true, message: "forbidden access" });
     }
 
     const query = { email: email };
     const result = await cartCollection.find(query).toArray();
     res.send(result);
   });
-  // role check 
+  // role check
 
-   app.get('/users/role/:email', verifyJWT, async (req, res) => {
-      const email = req.params.email;
-      
+  app.get("/users/role/:email", verifyJWT, async (req, res) => {
+    const email = req.params.email;
 
-      if (req.decoded.email !== email) {
-        return res.status(403).send({ error: true, message: 'forbidden access' })
-      }
+    if (req.decoded.email !== email) {
+      return res.status(403).send({ error: true, message: "forbidden access" });
+    }
 
-      const query = { email: email }
-      const user = await usersCollection.findOne(query);
-      // let result;
-      // if( user?.role === 'admin'){
-        
-      //   result = { admin: user?.role === 'admin' }
-      // }
-      // else if( user?.role === 'user'){
-      //    console.log(user);
-      //   result = { user: user?.role === 'user' }
-      // }
-      // else  if( user?.role === 'instructor'){
-      //   result = { instructor: user?.role === 'instructor' }
-      // }
-      // else{
-      // result= "unknown";
-      // }
-     
-      res.send(user);
-    })
-    // delete cart items 
-    app.delete('/carts/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await cartCollection.deleteOne(query);
-      res.send(result);
-    })
+    const query = { email: email };
+    const user = await usersCollection.findOne(query);
+    // let result;
+    // if( user?.role === 'admin'){
+
+    //   result = { admin: user?.role === 'admin' }
+    // }
+    // else if( user?.role === 'user'){
+    //    console.log(user);
+    //   result = { user: user?.role === 'user' }
+    // }
+    // else  if( user?.role === 'instructor'){
+    //   result = { instructor: user?.role === 'instructor' }
+    // }
+    // else{
+    // result= "unknown";
+    // }
+
+    res.send(user);
+  });
+  // delete cart items
+  app.delete("/carts/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await cartCollection.deleteOne(query);
+    res.send(result);
+  });
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -147,11 +173,10 @@ app.post('/users', async (req, res) => {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
